92579992000e2a84d4d60d66c8b2810cf5be11a2
Sébastien Fabbro <sebfabbro@gmail.com>
2023-09-13 16:50:48 +0000

    remove astrapids for now due to difficult building

--- build-all.sh
+++ build-all.sh
@@ -3,18 +3,25 @@
 source build.sh
 
 build_container base
-build_container base-gpu
-
 build_container astroml base
 build_container astroml-notebook astroml
 build_container astroml-vscode astroml
 
+build_container base-gpu
 build_container astroml-gpu base-gpu
 build_container astroml-gpu-notebook astroml-gpu
 build_container astroml-gpu-vscode astroml-gpu
 
-build_container astrapids-gpu base-gpu
-build_container astrapids-gpu-notebook astrapids-gpu
-build_container astrapids-gpu-vscode astrapids-gpu
+build_container automl base
+build_container automl-notebook automl
+build_container automl-gpu base-gpu
+build_container automl-gpu-notebook automl-gpu
+
+build_container astroflow base
+build_container astrapids-gpu base
+
+#build_container astrapids-gpu base-gpu
+#build_container astrapids-gpu-notebook astrapids-gpu
+#build_container astrapids-gpu-vscode astrapids-gpu
 
 
