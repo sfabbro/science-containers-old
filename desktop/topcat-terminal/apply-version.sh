05fc6211bc4dcb641c7fc3e3bacc8bc7b623f6c9
Mark Taylor <m.b.taylor@bristol.ac.uk>
2023-11-20 11:24:33 +0000

    topcat containers now pull latest public release
    Some changes made to the Dockerfile and apply-version.sh script for
    topcat and topcat-terminal packages.
    
    - The Dockerfile now downloads only topcat-extra to /usr/bin;
    its previous behaviour was to pull topcat-extra to /usr/local and
    topcat-full to /usr/bin.  topcat-extra does everything that topcat-full
    does (plus parquet) so there is no point having both.
    
    - The Dockerfile downloads the most recent public release, rather than
    thinking it's got a specific numbered release.
    
    - The apply-version.sh script now extracts the correct version from the
    image rather than using a hard-coded version number in the VERSION
    file.  The VERSION files, which were used in apply-version.sh,
    have been deleted since it's easier to do it all in the apply-version.sh
    script.

--- desktop/topcat-terminal/apply-version.sh
+++ desktop/topcat-terminal/apply-version.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-. VERSION && echo "tags: $TAGS"
+
+topcatimage=images.canfar.net/skaha/topcat-terminal
+
+# Extract topcat version from image.
+VERSION=`docker run ${topcatimage}:latest topcat -version | grep -i TOPCAT.version | sed 's/.*ersion //'`
+
+# Prepare tags: topcat version and build version (timestamp).
+TAGS="$VERSION $(date -u +'%Y%m%dT%H%M%S')"
+echo "tags: $TAGS"
+
+# Tag image.
 for t in $TAGS; do
-   docker image tag images.canfar.net/skaha/topcat-terminal:latest images.canfar.net/skaha/topcat-terminal:$t
+   docker image tag ${topcatimage}:latest ${topcatimage}:$t
 done
 unset TAGS
-docker image list images.canfar.net/skaha/topcat-terminal
+unset VERSION
+docker image list ${topcatimage}
--- desktop/topcat/apply-version.sh
+++ desktop/topcat/apply-version.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-. VERSION && echo "tags: $TAGS"
+
+topcatimage=images.canfar.net/skaha/topcat
+
+# Extract topcat version from image.
+VERSION=`docker run ${topcatimage}:latest topcat -version | grep -i TOPCAT.version | sed 's/.*ersion //'`
+
+# Prepare tags: topcat version and build version (timestamp).
+TAGS="$VERSION $(date -u +'%Y%m%dT%H%M%S')"
+echo "tags: $TAGS"
+
+# Tag image.
 for t in $TAGS; do
-   docker image tag images.canfar.net/skaha/topcat:latest images.canfar.net/skaha/topcat:$t
+   docker image tag ${topcatimage}:latest ${topcatimage}:$t
 done
 unset TAGS
-docker image list images.canfar.net/skaha/topcat
+unset VERSION
+docker image list ${topcatimage}
