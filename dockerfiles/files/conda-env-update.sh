a634c9d536f1eedc44fd8cc4107aeb425ed57a29
Sébastien Fabbro <sebfabbro@gmail.com>
2024-01-06 00:02:55 +0000

    add orb, vscode, and script conda updates

--- dockerfiles/files/conda-env-update.sh
+++ dockerfiles/files/conda-env-update.sh
@@ -6,4 +6,4 @@ mamba update --all --yes
 if [[ $(diff /build-info/env.yml ${yml} &> /dev/null) != 0 ]]; then
     mamba env update -n base --file ${yml}
 fi
-mamba clean --all --quiet --force --yes
+conda clean --all --quiet --force --yes
