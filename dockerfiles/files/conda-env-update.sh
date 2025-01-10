3a7f097e132e7b9720f6845a8863acbe7158ecd9
Sébastien Fabbro <sebfabbro@gmail.com>
2025-01-10 22:56:31 +0000

    updated dockerfiles

--- dockerfiles/files/conda-env-update.sh
+++ dockerfiles/files/conda-env-update.sh
@@ -6,4 +6,4 @@ mamba update --all --yes
 if [[ $(diff /build-info/env.yml ${yml} &> /dev/null) != 0 ]]; then
     mamba env update -n base --file ${yml}
 fi
-conda clean --all --quiet --force --yes
+mamba clean --all --quiet --yes
